da63ef275585238268c0a3a09af555bdf39f9a16
Jay Sanap <90241943+JaySanap@users.noreply.github.com>
2021-09-10 22:24:18 +0530

    Add files via upload

---
+++ JAY_CH_intornot.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class JAY_CH_intornot {
+    public static void main(String[] args) {
+        System.out.println("Enter your number");
+        Scanner sc=new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+}
---
+++ JAY_CH_sum.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class JAY_CH_sum {
+    public static void main(String[] args){
+        int a=4;
+        int b=7;
+        int c=6;
+        int sum=a+b+c;
+        System.out.println(sum);
+    }
+}
