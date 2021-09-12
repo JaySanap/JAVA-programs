89212176e886e48abe0693e583c5fad0bdb85958
Jay Sanap <90241943+JaySanap@users.noreply.github.com>
2021-09-12 10:40:25 +0530

    Add files via upload

---
+++ JAY_PRESETQ4.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class JAY_PRESETQ4 {
+    public static void main(String[] args) {
+        float a=4;
+        float s=2;
+        float v=8;
+        float u=6;
+        float k=(8*8-6*6)/(2.0f*4*2);
+        System.out.println(k);
+    }
+}
