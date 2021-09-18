3549ef864c1c843f88ea3c5b60dc4a330c292595
Jay Sanap <90241943+JaySanap@users.noreply.github.com>
2021-09-18 11:38:11 +0530

    Add files via upload

---
+++ JAY_PRESET5Q3EX.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class JAY_PRESET5Q3EX {
+    public static void main(String[] args) {
+        int n=10;
+        for (int i=1;i<=10;i++){
+            System.out.printf(" %d * %d =%d\n",n,i,n*i);
+        }
+
+    }
+}
---
+++ JAY_PRESET5Q4.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class JAY_PRESET5Q4 {
+    public static void main(String[] args) {
+        int n=10;
+        for (int i=10;i>=1;i--){
+            System.out.printf(" %d * %d =%d\n",n,i,n*i);
+        }
+    }
+}
